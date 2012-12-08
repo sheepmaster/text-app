7959be28649942ccb34a0cd4b60b4b090efb80d7
Bernhard Bauer <bauerb@google.com>
2012-12-08 18:34:11 +0100

    Add webDAVFs stub

---
+++ app/ts/webDAVFs.ts
@@ -0,0 +1,166 @@
+module webDAVFs {
+
+  export class FileSystem {
+    name: string;
+
+    root: DirectoryEntry;
+
+    static create(url: string): FileSystem {
+      return new FileSystemImpl(url, url);
+    }
+  }
+
+  export interface ErrorCallback {
+    (any);
+  }
+
+  export interface DirectoryReader {
+    readEntries (successCallback?: (entries: Entry[]) => void,
+                 errorCallback?: ErrorCallback): void;
+  }
+
+  export interface Flags {
+    create?: bool;
+    exclusive?: bool;
+  }
+
+  export interface Metadata {
+    modificationTime: Date;
+    size: number;
+  };
+
+  export interface Entry {
+    isFile: bool;
+    isDirectory: bool;
+    getMetadata(successCallback: (Metadata) => void,
+                errorCallback?: ErrorCallback);
+    name: string;
+    fullPath: string;
+    filesystem: FileSystem;
+    moveTo(parent: DirectoryEntry,
+           newName?: string,
+           successCallback?: (entry: Entry) => void,
+           errorCallback?: ErrorCallback);
+    copyTo(parent: DirectoryEntry,
+           newName?: string,
+           successCallback?: (entry: Entry) => void,
+           errorCallback?: ErrorCallback);
+    toURL(): string;
+    remove(successCallback: () => void, errorCallback?: ErrorCallback);
+    getParent(successCallback: (entry: Entry) => void,
+              errorCallback?: ErrorCallback);
+  }
+
+  export interface DirectoryEntry extends Entry {
+    createReader(): DirectoryReader;
+    getFile(path: string,
+            options?: Flags,
+            successCallback?: (entry: Entry) => void,
+            errorCallback?: ErrorCallback): void;
+    getDirectory(path: string,
+                 options?: Flags,
+                 successCallback?: (entry: Entry) => void,
+                 errorCallback?: ErrorCallback): void;
+    removeRecursively(successCallback?: () => void,
+                      errorCallback?: ErrorCallback): void;
+  }
+
+  export interface FileEntry extends Entry {
+    createWriter(successCallback: (FileWriter) => void,
+                 errorCallback?: ErrorCallback);
+    file(successCallback: (File) => void,
+         errorCallback?: ErrorCallback);
+  }
+
+  /* Implementation */
+
+  class FileSystemImpl extends FileSystem {
+    url_: string;
+
+    constructor(url: string, public name: string) {
+      super();
+      this.url_ = url;
+      this.root = new DirectoryEntryImpl(this, '/', '/');
+    }
+  }
+
+  class DirectoryEntryImpl implements DirectoryEntry {
+    isFile = false;
+    isDirectory = true;
+
+    constructor(public filesystem: FileSystem,
+                public name: string,
+                public fullPath: string) {
+    }
+
+    getMetadata(successCallback: (Metadata) => void,
+                errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    moveTo(parent: DirectoryEntry,
+           newName?: string,
+           successCallback?: (entry: Entry) => void,
+           errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    copyTo(parent: DirectoryEntry,
+           newName?: string,
+           successCallback?: (entry: Entry) => void,
+           errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    toURL(): string {
+      throw new Error('Not implemented');
+      return '';
+    }
+
+    remove(successCallback: () => void, errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    getParent(successCallback: (entry: Entry) => void,
+              errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    createReader() {
+      return {
+        'readEntries': function(successCallback?: (entry: Entry[]) => void,
+                                errorCallback?: ErrorCallback) {
+          if (errorCallback)
+            errorCallback(new Error('Not implemented'));
+        }
+      };
+    }
+
+    getFile(path: string,
+            options?: Flags,
+            successCallback?: (entry: Entry) => void,
+            errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    getDirectory(path: string,
+                 options?: Flags,
+                 successCallback?: (entry: Entry) => void,
+                 errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+
+    removeRecursively(successCallback?: () => void,
+                      errorCallback?: ErrorCallback) {
+      if (errorCallback)
+        errorCallback(new Error('Not implemented'));
+    }
+  }
+}
